57d5b9670a36bbc1b182c13d4d48634812c5d7a4
SrLozano <100383511@alumnos.uc3m.es>
2020-05-06 23:17:41 +0200

    Minor fixes

--- filesystem/metadata.h
+++ filesystem/metadata.h
@@ -36,8 +36,6 @@ struct Superblock;
 
 typedef struct TipoSuperbloque{
     unsigned int numMagico;	                   /* Número mágico del superbloque: 0x000D5500 */
-    //unsigned int numBloquesMapaInodos;         /* Número de bloques del mapa inodos */
-    //unsigned int numBloquesMapaDatos;          /* Número de bloques del mapa  datos */
     unsigned int numInodos; 	                 /* Número de inodos en el dispositivo */
     unsigned int primerInodo;	                 /* Número del bloque del 1º inodo del disp. (inodo raíz) */
     unsigned int numBloquesDatos;              /* Número de bloques de datos en el disp. */
@@ -45,7 +43,7 @@ typedef struct TipoSuperbloque{
     unsigned int tamDispositivo;	             /* Tamaño total del disp. (en bytes) */
     char i_map[NUMINODO/8] ; 			             /* Mapa de bits con los inodos (usado: i_map[x]=1 | libre: i_map[x]=0) */ 
     char b_map[NUMBLOQUESDATO/8] ; 	           /* Mapa de bits con los bloques (usado: b_map[x]=1 | libre: b_map[x]=0) */
-    char relleno[BLOCK_SIZE-8*sizeof(int)-NUMINODO/8 -NUMBLOQUESDATO/8];    /* Campo de relleno (para completar un bloque entero en el superbloque) */
+    char relleno[BLOCK_SIZE-6*sizeof(unsigned int)-NUMINODO/8 -NUMBLOQUESDATO/8];    /* Campo de relleno (para completar un bloque entero en el superbloque) */
 } TipoSuperbloque;
 
 
